87690d956cbb832d56a223bdf84369b7bc23fc1e
cornie <cornie.wiens@vidir.com>
2019-09-16 16:54:56 -0500

    Setup classes and methods for async testing

--- ASyncTest/ASyncTest/Program.cs
+++ ASyncTest/ASyncTest/Program.cs
@@ -10,8 +10,120 @@ namespace ASyncTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
-            Console.ReadLine();
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+            Egg eggs = FryEggs(2);
+            Console.WriteLine("eggs are ready");
+            Bacon bacon = FryBacon(3);
+            Console.WriteLine("bacon is ready");
+            Toast toast = ToastBread(2);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            Console.WriteLine("Breakfast is ready!");
+            Console.ReadKey();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            SimulateWork(1000);
+            return new Coffee(100);
+        }
+
+        private static Egg FryEggs(int eggCount)
+        {
+            SimulateWork(1000);
+            return new Egg(eggCount);
+        }
+
+        private static Bacon FryBacon(int count)
+        {
+            SimulateWork(1000);
+            return new Bacon(count);
+        }
+
+        private static Toast ToastBread(int slices)
+        {
+            SimulateWork(1000);
+            return new Toast(false, false);
+        }
+
+        private static Juice PourOJ()
+        {
+            SimulateWork(1000);
+            return new Juice("Orange");
+        }
+
+        private static void ApplyButter(Toast toast)
+        {
+            SimulateWork(1000);
+            toast.HasButter = true;
+        }
+
+        private static void ApplyJam(Toast toast)
+        {
+            SimulateWork(1000);
+            toast.HasJam = true;
+        }
+
+        private static void SimulateWork(int ms)
+        {
+            System.Threading.Thread.Sleep(ms);
+        }
+    }
+
+    public class Coffee
+    {
+        public int Temp { get; set; }
+
+        public Coffee(int temp)
+        {
+            Temp = temp;
+        }
+    }
+
+    public class Egg
+    {
+        public int EggCount { get; set; }
+
+        public Egg(int eggCount)
+        {
+            EggCount = eggCount;
+        }
+    }
+
+    public class Bacon
+    {
+        public int BaconCount { get; set; }
+
+        public Bacon(int baconCount)
+        {
+            BaconCount = baconCount;
+        }
+    }
+
+    public class Toast
+    {
+        public bool HasButter { get; set; }
+        public bool HasJam { get; set; }
+
+        public Toast(bool hasButter, bool hasJam)
+        {
+            HasButter = hasButter;
+            HasJam = hasJam;
+        }
+    }
+
+    public class Juice
+    {
+        public string Type { get; set; }
+
+        public Juice(string type)
+        {
+            Type = type;
         }
     }
 }
